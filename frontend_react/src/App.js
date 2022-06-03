0e2a7f936c24a73f5aa2517f0e87ac8eb95a6850
Alberto Ginel Salvador <aginelsalvador@gmail.com>
2022-06-03 10:42:30 +0100

    Navbar

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { About, Header, Footer, Skills, Testimonial, Work} from "./container/index";
 import { Navbar } from './components';
+import './App.scss'
 
 const App = () => {
   return (
